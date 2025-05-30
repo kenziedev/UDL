717838e4fcd9bf29e0479e1ab3765b58b6fd7ea1
kenzie <kenziedev90@gmail.com>
2025-05-30 23:25:17 +0900

    Updates

--- udlAi.bundle.js
+++ udlAi.bundle.js
@@ -6,9 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+/******/ var __webpack_modules__ = ({
 
 /***/ 95:
 /***/ (() => {
@@ -17,14 +15,12 @@ eval("function _createForOfIteratorHelper(r, e) { var t = \"undefined\" != typeo
 
 /***/ })
 
-/******/ 	});
+/******/ });
 /************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__[95]();
-/******/ 	
-/******/ })()
-;
+/******/ 
+/******/ // startup
+/******/ // Load entry module and return exports
+/******/ // This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ var __webpack_exports__ = {};
+/******/ __webpack_modules__[95]();
+/******/ 
